8be4250a3234a5c1a50e4d7ff2882b77b06f41ab
Wojciech Rogóż <wrogoz@gmail.com>
2019-07-04 09:55:36 +0200

    index route created

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -5,6 +5,8 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const url = "mongodb+srv://test:test@camps-un0gw.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(url, {useNewUrlParser: true});
 
+
+
 const blogSchema =new mongoose.Schema({
     title:String,
     image:String,
@@ -18,11 +20,23 @@ const Blog = mongoose.model( 'Blog', blogSchema );
 
 module.exports = (app)=>{
 
-
+   
+//RESTfull ROUTES
  
-    app.get('/', (req, res) => {
 
-        res.send('Hello World!')
+    app.get('/', (req,res)=>{
+       res.redirect('/blogs');
+    })
+
+    app.get('/blogs', (req, res) => {
+        Blog.find({},(err,blogs)=>{
+            if(err){
+                console.log('db error')
+            }else{
+                res.render('index',{blogs:blogs})
+            }
+        })
+        
 
     });
 
